ab615568fc288c4abd4db6bb3ff971626090a0c0
cristianale09 <cristianale09@outlook.com>
2025-11-14 23:31:01 -0300

    modificacion en los html de productos y creacion del archivo js de productos

---
+++ assets/script-products.js
@@ -0,0 +1,142 @@
+const productos = [
+
+    /* Gabinetes */
+
+    {
+        id: 1,
+        nombre: "Corsair_4000d",
+        precio: 250.00,
+    },
+    {
+        id: 2,
+        nombre: "Corsair_2500d",
+        precio: 300.00,
+    },
+    {
+        id: 3,
+        nombre: "Corsair_Carbide",
+        precio: 200.00,
+    },
+    {
+        id: 4,
+        nombre: "Corsair_4000d_v2",
+        precio: 190.00,
+    },
+
+    /* Memorias */
+
+    {
+        id: 5,
+        nombre: "adata_16gb",
+        precio: 59.999,
+    },
+    {
+        id: 6,
+        nombre: "adata_xpg_16gb",
+        precio: 79.999,
+    },
+    {
+        id: 7,
+        nombre: "adata_xpg-rgb_16gb",
+        precio: 129.999,
+    },
+    {
+        id: 8,
+        nombre: "kingston_fury_16gb",
+        precio: 89.999,
+    },
+
+    /* Monitores */
+
+    {
+        id: 9,
+        nombre: "Viewsonic_27_qhd",
+        precio: 199.999,
+    },
+    {
+        id: 10,
+        nombre: "Lg_ultragear_27_fhd",
+        precio: 229.999,
+    },
+    {
+        id: 11,
+        nombre: "Lg_ultraGear_32_qhd",
+        precio: 259.999,
+    },
+    {
+        id: 12,
+        nombre: "Samsung_odyssey_25fhd",
+        precio: 189.999,
+    },
+
+    /* Motherboard */
+
+    {
+        id: 13,
+        nombre: "Asus_tuf",
+        precio: 219.999,
+    },
+    {
+        id: 14,
+        nombre: "Gigabyte_b840m",
+        precio: 219.999,
+    },
+    {
+        id: 15,
+        nombre: "Gigabyte_b850m",
+        precio: 249.999,
+    },
+    {
+        id: 16,
+        nombre: "Msi_z890",
+        precio: 189.999,
+    },
+
+    /* Placa de video */
+
+    {
+        id: 17,
+        nombre: "Gigabyte_5060ti",
+        precio: 419.999,
+    },
+    {
+        id: 18,
+        nombre: "MSI_5090",
+        precio: 729.999,
+    },
+    {
+        id: 19,
+        nombre: "MSI_5050",
+        precio: 329.999,
+    },
+    {
+        id: 20,
+        nombre: "Gigabyte_RTX_5070",
+        precio: 489.999,
+    },
+
+    /* Teclado y Mouse */
+
+    {
+        id: 21,
+        nombre: "NKB_68",
+        precio: 119.999,
+    },
+    {
+        id: 22,
+        nombre: "RedDragon_d1900",
+        precio: 129.999,
+    },
+    {
+        id: 23,
+        nombre: "Shenlong_4000x",
+        precio: 159.999,
+    },
+    {
+        id: 24,
+        nombre: "Logitech_G502",
+        precio: 89.999,
+    }
+]
+
+let carrito = [];
